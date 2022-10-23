8236c75d642d9bd14c8c0f414c5025c12259b16c
JeevanNaikOp <naikjeevan97@gmail.com>
2022-10-23 14:24:45 +0530

    Created ship class, made some changes to alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship 
 
 class AlienInvasion:
     def __init__(self):
@@ -14,17 +15,21 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """ Start the main game """
         while True:
             # checking keyboard and mouse
             for event in pygame.event.get():
-                if event.type == pygame.quit:
+                if event.type == pygame.QUIT:
                     sys.exit()
 
             # fill the background
             self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
+
             # make the most recent drawn on screen visible
             pygame.display.flip()
 
