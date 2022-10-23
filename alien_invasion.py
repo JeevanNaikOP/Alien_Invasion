f2b5bde46d87146d40865178b5afb28ee51ae2ae
JeevanNaikOp <naikjeevan97@gmail.com>
2022-10-23 13:51:42 +0530

    created new class Settings, added changes relevent to it

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         """ initialize pygame """
         pygame.init()
 
-        pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien invasion")
-
         self.clock = pygame.time.Clock()
 
-        # set background color
-        self.bg_color = (230,230,230)
+        # set background color and window
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien invasion")
 
     def run_game(self):
         """ Start the main game """
@@ -23,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
 
             # fill the background
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # make the most recent drawn on screen visible
             pygame.display.flip()
 
