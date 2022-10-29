224dcefdc447d3f5c33afe845675f34d6c2447f2
JeevanNaikOp <naikjeevan97@gmail.com>
2022-10-29 18:59:16 +0530

    fixed bug, multiple rows of alien are visible

--- alien_invasion.py
+++ alien_invasion.py
@@ -115,18 +115,22 @@ class AlienInvasion:
         current_x = alien_width
         current_y = alien_height
 
-        while current_y < (self.settings.screen_height):
+        while current_y < (self.settings.screen_height - 3 * alien_height):
             while current_x < (self.settings.screen_width ):
-                self._create_alien(current_x)
-                current_x += 2* alien_width
+                self._create_alien(current_x,current_y)
+                current_x += 2 * alien_width
             
-            current_y += 2* alien_height
+            current_y += 2 * alien_height
 
-    def _create_alien(self,current_x):
+            # reset current_x to start from left
+            current_x = alien_width
+
+    def _create_alien(self,current_x,current_y):
         """ Create an alien and place it in the row """
         new_alien = Alien(self)
         new_alien.x = current_x
         new_alien.rect.x = current_x
+        new_alien.rect.y = current_y
 
         self.aliens.add(new_alien)
 
