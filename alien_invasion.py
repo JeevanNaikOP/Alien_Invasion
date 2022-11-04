cebf103858568ed905be07794440b8a465ee7a21
JeevanNaikOp <naikjeevan97@gmail.com>
2022-11-04 20:36:34 +0530

    bug fix

--- alien_invasion.py
+++ alien_invasion.py
@@ -149,10 +149,10 @@ class AlienInvasion:
         """ Check if aliens have reached edge """
         for alien in self.aliens.sprites():
             if alien.check_edges():
-                self._change_fleet_direection()
+                self._change_fleet_direction()
                 return
 
-    def _change_fleet_direection(self):
+    def _change_fleet_direction(self):
         """ drop the fleet and change direction """
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
