564ec8ee27ff6ff0b136b3f7c90b6d4eee73d925
JeevanNaikOp <naikjeevan97@gmail.com>
2022-11-12 12:10:47 +0530

    feat: editing button class

--- button.py
+++ button.py
@@ -7,3 +7,25 @@ class Button:
         """ Initialize button attribute """
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
+
+        # set the dimensio of button
+        self.width = 200
+        self.height = 50
+
+        self.button_color = (0,135,0)
+        self.text_color = (255,255,255)
+
+        self.font = pygame.font.SysFont(None,48)
+
+        # build buttons rect
+        self.rect = pygame.Rect(0,0,self.width,self.height)
+        self.rect.center = self.screen_rect.center
+
+        # button message
+        self._prep_msg(msg)
+
+    def _prep_msg(self,msg):
+        """ Turn message into rendered image and center it """
+        self.msg_img = self.font.render(msg,True,self.text_color,self.button_color)
+        self.msg_img_rect = self.msg_img.get_rect()
+        self.msg_img_rect.center = self.rect.center
