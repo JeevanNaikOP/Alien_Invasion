ec7c8bd48e7ad03a9a3e6ecad2b5f1f1b70c3149
JeevanNaikOp <naikjeevan97@gmail.com>
2022-11-21 19:52:33 +0530

    feat: added level functionality

--- scoreboard.py
+++ scoreboard.py
@@ -20,6 +20,9 @@ class Scoreboard:
         # score high score
         self.prep_high_score()
 
+        # dispaly level
+        self.prep_level()
+
     def _prep_msg(self):
         """ Turn score into rendered image  """
         rounded_score = round(self.stats.score,-1)
@@ -37,6 +40,7 @@ class Scoreboard:
         """ Display score on the screen """
         self.screen.blit(self.score_img,self.score_img_rect)
         self.screen.blit(self.high_score_img,self.high_score_img_rect)
+        self.screen.blit(self.level_img,self.level_img_rect)
 
     def prep_high_score(self):
         """ Turn the highscore into rendered image """
@@ -56,3 +60,14 @@ class Scoreboard:
         if self.stats.score > self.stats.high_score:
             self.stats.high_score = self.stats.score
             self.prep_high_score()
+
+    def prep_level(self):
+        """ Turn the level into rendered image """
+        level_str = str(self.stats.level)
+
+        self.level_img = self.font.render(level_str,True,self.text_color,self.settings.bg_color)
+        
+        # Display imag at top right of the screen
+        self.level_img_rect = self.level_img.get_rect()
+        self.level_img_rect.right = self.score_img_rect.right
+        self.level_img_rect.top = self.score_img_rect.bottom + 10
